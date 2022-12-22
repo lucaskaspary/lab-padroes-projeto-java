d4c43e12065331e08508858c7d3373abe535b7a4
lucaskaspary <lucaskaspary@hotmail.com>
2022-12-22 14:35:00 -0300

    Initial commit

---
+++ src/main/java/com/lucas/designpatterns/LabPadroesProjetoJavaApplication.java
@@ -0,0 +1,13 @@
+package com.lucas.designpatterns;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabPadroesProjetoJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabPadroesProjetoJavaApplication.class, args);
+    }
+
+}
